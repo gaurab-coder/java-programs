758341c01e69763730434df79b68a314e706469d
gaurab <gaurabkhadka332@gmail.com>
2025-12-27 13:19:17 +0545

    for each loop

---
+++ ForEachLoop.java
@@ -0,0 +1,11 @@
+class ForEachLoop
+{
+    public static void main(String[] ae)
+    {
+        String[] vegetables={"Carrot","Broccoli","Spinach","Cauliflower","Cabbage"};
+        for (String veg:vegetables)
+        {
+            System.err.println(veg);
+        }
+    }
+}
