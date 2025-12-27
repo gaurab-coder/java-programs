7bb2f913259fb6750143b93eb5fd254d0e44a172
gaurab <gaurabkhadka332@gmail.com>
2025-12-27 13:16:23 +0545

    loop

---
+++ WhileLoop.java
@@ -0,0 +1,12 @@
+class WhileLoop
+{
+    public static void main(String []eww)
+    {
+        int a=10;
+        while(a<18)
+        {
+            System.err.println(""+a);
+            a++;
+        }
+    }
+}
