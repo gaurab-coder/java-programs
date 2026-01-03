d87c1b656345e56e552cf4d0917d002450dc555f
gaurab <gaurabkhadka332@gmail.com>
2026-01-03 10:00:42 +0545

    multilevel inheritance

---
+++ MultilevelInheritanceExample2.java
@@ -0,0 +1,34 @@
+class Vehicle
+{
+    void start()
+    {
+        System.out.println("Vehicle is starting");
+    }
+}
+
+class Car extends Vehicle
+{
+    void drive()
+    {
+        System.out.println("Car is driving");
+    }
+}
+
+class ElectricCar extends Car
+{
+    void charge()
+    {
+        System.out.println("Electric car is charging");
+    }
+}
+
+public class MultilevelInheritanceExample2
+{
+    public static void main(String[] args)
+    {
+        ElectricCar obj = new ElectricCar();
+        obj.start();
+        obj.drive();
+        obj.charge();
+    }
+}
