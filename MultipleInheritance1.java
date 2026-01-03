9171b24eb82667509a86620a1606cf1fdea592bd
gaurab <gaurabkhadka332@gmail.com>
2026-01-03 10:03:21 +0545

    multiple inheritance

---
+++ MultipleInheritance1.java
@@ -0,0 +1,32 @@
+interface A
+{
+    void showA();
+}
+
+interface B
+{
+    void showB();
+}
+
+class C implements A, B
+{
+    public void showA()
+    {
+        System.out.println("Method from interface A");
+    }
+
+    public void showB()
+    {
+        System.out.println("Method from interface B");
+    }
+}
+
+public class MultipleInheritance1
+{
+    public static void main(String[] args)
+    {
+        C obj = new C();
+        obj.showA();
+        obj.showB();
+    }
+}
