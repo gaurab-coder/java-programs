5ef83cce27768c9841d7409487a410d606fc7436
gaurab <gaurabkhadka332@gmail.com>
2026-01-05 09:27:38 +0545

    object class

---
+++ ObjectClassDemo.java
@@ -0,0 +1,14 @@
+public class ObjectClassDemo
+{
+    public static void main(String [] ojfh)
+    {
+        Object s=new String(" Welcome to java world!");
+        Object i=new Integer(10);
+        Object d=new Double(10.99);
+        Object f=new Float(99.98);
+        System.err.println("String value:"+s);
+        System.err.println("String value:"+i);
+        System.err.println("String value:"+d);
+        System.err.println("String value:"+f);
+    }
+}
