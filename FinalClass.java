76ff2d71b3771698ff5beb08e1ca8b9bf36c0d9c
gaurab <gaurabkhadka332@gmail.com>
2026-01-05 09:29:19 +0545

    final class

---
+++ FinalClass.java
@@ -0,0 +1,15 @@
+final class Admin
+{
+    void display()
+    {
+        System.err.println("java project");
+    }
+}
+public class FinalClass extends Admin
+{
+    public static void main(String [] sdh)
+    {
+        Admin obj=new Admin();
+        obj.display();
+    }
+}
