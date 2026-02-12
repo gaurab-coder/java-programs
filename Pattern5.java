c0838deabe77ebc9f7aa3ba6c375197beb383880
Gaurab Khadka <gaurabkhadka332@gmail.com>
2026-02-12 18:42:49 +0545

    pattern program

---
+++ Pattern5.java
@@ -0,0 +1,34 @@
+// To print like the following:
+// *****
+//  ****
+//   ***
+//    **
+//     *
+import java.util.*;
+public class Pattern5
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the value of n:");
+        int n = sc.nextInt();
+        // i loop : iterating n times
+        for (int i = 1; i <= n; i++)
+        {
+             // Printing the spaces
+            // iterating i-1 times
+            for (int j = 1; j <= i-1; j++)
+            {
+                System.out.print(" "); 
+            }
+             // Printing the stars
+            // iterating n-i+1 times
+            for (int j = 1; j <= n-i+1; j++)
+            {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
+
