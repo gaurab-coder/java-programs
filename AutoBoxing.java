aae46b61d6c7bba1cbb2eeef771db7c389b81603
Gaurab Khadka <gaurabkhadka332@gmail.com>
2026-03-01 09:05:25 +0545

    auto boxing

---
+++ AutoBoxing.java
@@ -0,0 +1,9 @@
+public class AutoBoxing
+{
+    public static void main(String[] afdj)
+    {
+        int i=34;
+        Integer intobj= new Integer(i);
+        System.out.println(intobj);
+    }
+}
