b6a74107fdf055e7f775073b7d8847e3cb79a3cc
Gaurab Khadka <gaurabkhadka332@gmail.com>
2026-03-01 09:01:17 +0545

    Math in java

---
+++ JavaLangMathDemo.java
@@ -0,0 +1,17 @@
+public class JavaLangMathDemo
+{
+    public static void main(String [] afkej)
+    {
+        float f= 4;
+        float g=7;
+        System.out.println("max of f and g is "+Math.max(f,g));
+        System.out.println("min of f and g is "+Math.min(f,g));
+        System.out.println("square root of f is "+Math.sqrt(f));
+        System.out.println("square root of g is "+Math.sqrt(g));
+        System.out.println("power of f and g is "+Math.pow(f,g));
+        System.out.println("logarithmn of f is "+Math.log(f));
+        System.out.println("log10  of g is "+Math.log10(g));
+        System.out.println("exp of f is "+Math.exp(f));
+        System.out.println("expm of g is "+Math.expm1(g));
+    }
+}
