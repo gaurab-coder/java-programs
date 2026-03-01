d598e45bcc544e9166ebc8606486bb8456f70e33
Gaurab Khadka <gaurabkhadka332@gmail.com>
2026-03-01 08:52:11 +0545

    Wrapper

---
+++ WrappersClassObjectsDemo.java
@@ -0,0 +1,15 @@
+public class WrappersClassObjectsDemo {
+    public static void main(String[] ajf)
+    {
+        String str = "55";
+
+        int i = Integer.parseInt(str);
+        System.out.println(i);
+
+        double d = Double.parseDouble(str);
+        System.out.println(d);
+
+        boolean b = Boolean.parseBoolean(str);
+        System.out.println(b);
+    }
+}
