77a28bc5915ac71b4f3e171d7518bfa4f090c529
Gaurab Khadka <gaurabkhadka332@gmail.com>
2026-03-13 22:16:37 +0545

    applet demo

---
+++ AppletBasic.java
@@ -0,0 +1,9 @@
+import java.applet.Applet;
+import java.awt.Graphics;
+public class AppletBasic extends Applet
+{
+    public void paint(Graphics g)
+    {
+        g.drawString("Gaurab",100,50);
+    }
+}
